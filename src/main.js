6cfa74a5c94d2186beb19c60d62740a258719c8c
MilagroTheBuoy <milagrodakid@gmail.com>
2023-10-05 17:26:16 +0200

    HUD working, but it has a few bugs

--- src/main.js
+++ src/main.js
@@ -20,6 +20,29 @@ let ground
 const playerSpeed = 0.0325
 const playerPos = new THREE.Vector3(0, 0, 0)
 const playerDir = new THREE.Vector3()
+
+//HUD
+var playerHealth = 100; // Player's actual health
+var maxHealth = 100; // Maximum health
+
+var playerHunger = 100; // Player's actual hunger level
+var maxHunger = 100; 
+
+const canvasHealth = document.getElementById("healthBar");
+const contextHealth = canvasHealth.getContext("2d");
+
+const canvasScore = document.getElementById("score");
+const contextScore = canvasScore.getContext("2d");
+
+const canvasTime = document.getElementById("time");
+const contextTime = canvasTime.getContext("2d");
+
+const canvasHunger = document.getElementById("hungerBar");
+const contextHunger = canvasHunger.getContext("2d");
+
+const endTime = new Date().getTime() + 5 * 60 * 1000;
+
+
 // ================================================================================================
 
 // F U N C T I O N S ==============================================================================
@@ -33,6 +56,9 @@ function animate(){
     camFollowPlayer()
     
     renderer.render(scene, camera)
+    setBarNumber();
+    drawTime();
+    //renderer.render(sceneHUD, cameraHUD);
     // movePlayer()
 }
 
@@ -79,6 +105,10 @@ const handleKeyUp = (e) => {
         case 'ArrowRight':
             playerDir.x = 0
             break
+        //
+        case 'j':
+            playerHunger = Math.max(100, playerHunger);
+            playerHunger = Math.min(playerHunger, 100);
     }
 }
 
@@ -108,12 +138,77 @@ function addPlane(){
     return plane
 }
 
+//MISO HUD STUFF
+
+function drawVariableBar(context, canvas, health, maxHealth, colour) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  
+    // Draw the background
+    context.fillStyle = "gray";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  
+    // Draw the health portion
+    const healthWidth = (health / maxHealth) * canvas.width;
+    context.fillStyle = colour;
+    context.fillRect(0, 0, healthWidth, canvasHealth.height);
+  }
+  
+function setTextStyle(context){
+    context.fillStyle = "white";
+    context.font = "20px Comic Sans MS";
+    context.strokeStyle = "white";
+}
+
+function drawScore(score){
+    setTextStyle(contextScore);
+    const stringScore = "Score: "+ score;
+    contextScore.strokeText(stringScore, 10, 20);
+    contextScore.fillText(stringScore, 10, 20);
+}
+
+function drawTime(){
+    contextTime.clearRect(0, 0, canvasTime.width, canvasTime.height);
+    setTextStyle(contextTime);
+    const stringTime = updateCountdown();
+    contextTime.strokeText(stringTime, 10, 20);
+    contextTime.fillText(stringTime, 10, 20);
+}
+
+function setBarNumber(){
+    if(playerHunger > 0){
+        playerHunger = Math.max(0, playerHunger - 0.15);
+    }
+    if(playerHunger <= 0){
+        playerHealth = Math.max(0, playerHealth - 0.05);
+    }
+    //console.log(playerHunger);
+    drawVariableBar(contextHealth, canvasHealth, playerHealth, maxHealth, "green");
+    drawVariableBar(contextHunger, canvasHunger, playerHunger, maxHunger, "orange");  
+}
+
+function updateCountdown() {
+    const currentTime = new Date().getTime();
+    const timeRemaining = endTime - currentTime;
+
+    if (timeRemaining <= 0) {
+      clearInterval(interval);
+      return 'Countdown expired!';
+      //document.getElementById('time').innerHTML = 'Countdown expired!';
+    } else {
+      const minutes = Math.floor((timeRemaining / 1000) / 60);
+      const seconds = Math.floor((timeRemaining / 1000) % 60);
+      `Time remaining: ${minutes} minutes ${seconds} seconds`
+      return `Time: 0${minutes}: ${seconds}`;
+    }
+}
+
 // ================================================================================================
 
 // M A I N ========================================================================================
 
 //Scene elements
 scene.add(ambientLight)
+//scene.add(health)
 
 directionalLight.position.set(0, 10, 0)
 scene.add(directionalLight)
@@ -129,6 +224,34 @@ scene.add(player)
 
 //Set camera pos
 camera.position.set(0, 1, 5)
+// Call this whenever the player's health changes
+
+//=============================================================
+//MISO STUFF
+
+
+
+// Example of updating the health bar
+// Maximum hunger
+
+// Call this whenever the player's health changes
+drawScore(80);
+canvasHealth.style.position = "absolute";
+canvasHealth.style.top = "10px";
+canvasHealth.style.left = "200px";
+
+canvasScore.style.position = "absolute";
+canvasScore.style.top = "10px";
+canvasScore.style.left = "10px";
+
+canvasTime.style.position = "absolute";
+canvasTime.style.top = "10px";
+canvasTime.style.right = "20px";
+
+canvasHunger.style.position = "absolute";
+canvasHunger.style.top = "60px";
+canvasHunger.style.left = "200px";
+//====================================================================
 
 //Call function
 animate()

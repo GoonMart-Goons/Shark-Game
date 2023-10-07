fd25d53913bcc9baa8ba1ae43a68e574656329b4
MilagroTheBuoy <milagrodakid@gmail.com>
2023-10-07 12:24:26 +0200

    Added skybox

--- src/main.js
+++ src/main.js
@@ -42,6 +42,29 @@ const contextHunger = canvasHunger.getContext("2d");
 
 const endTime = new Date().getTime() + 5 * 60 * 1000;
 
+//Skybox, texture from https://jkhub.org/files/file/3216-underwater-skybox/
+let materialArray = [];
+let texture_ft = new THREE.TextureLoader().load('./Images/uw_ft.jpg');
+let texture_bk = new THREE.TextureLoader().load('./Images/uw_bk.jpg');
+let texture_up = new THREE.TextureLoader().load('./Images/uw_up.jpg');
+let texture_dn = new THREE.TextureLoader().load('./Images/uw_dn.jpg');
+let texture_rt = new THREE.TextureLoader().load('./Images/uw_rt.jpg');
+let texture_lf = new THREE.TextureLoader().load('./Images/uw_lf.jpg');
+
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}));
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}));
+
+let skyBoxShape = new THREE.BoxGeometry(1000, 1000, 1000);
+let skyBox = new THREE.Mesh(skyBoxShape, materialArray);
+
+for (let i = 0; i < 6; i++){
+    materialArray[i].side = THREE.BackSide;
+}
+
 
 // ================================================================================================
 
@@ -220,6 +243,7 @@ ground.rotation.set(90, 0, 0)
 scene.add(ground)
 //Test cube as player for now
 player = addCube()
+scene.add(skyBox)
 scene.add(player)
 
 //Set camera pos
